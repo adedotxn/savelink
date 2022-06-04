1597648e280d788b5426949f22ffd417987e3072
Phyf3 <phade075@gmail.com>
2022-06-03 20:00:00 -0600

    feat : category can now be added ..fix: terrible TS code

--- pages/demo/dashboard.tsx
+++ pages/demo/dashboard.tsx
@@ -11,11 +11,13 @@ import MoreSvg from '../../components/svg/moresvg'
 
 interface IData {
     _id : number;
-    title : string;
+    title? : string;
     url : string;
-    category : string;
+    category? : string;
     starred : boolean
 }
+
+
 
 
 const Dashboard:NextPage = () => {
@@ -43,51 +45,67 @@ const Dashboard:NextPage = () => {
 
     function handleSubmit (e : FormEvent<HTMLFormElement>) {
         e.preventDefault()
+
+
         const data = new FormData(e.target as HTMLFormElement);
-        let nutitle :string = data.get("title")?.toString() || ''
-        let nulink:string =  data.get("link")?.toString()!
+        let inputedTitle :string = data.get("title")?.toString() || ''
+        let inputedLink:string =  data.get("link")?.toString()!
         const form : HTMLFormElement = document.forms['input-form'].categories;
-        console.log(form)
-        let latestID = DLinks[DLinks.length - 1]._id++
-        let nucategory :string = data.get("category")?.toString() || ''
+        let selectedCategory = form.value;
 
-        let category = form.value === "Choose..." ? nucategory : form.value;
 
-        
         const newDlinks :IData = {
             _id : Math.random(),
-            title : nutitle,
-            url: nulink,
-            category: category,
+            title : inputedTitle,
+            url: inputedLink,
+            category: selectedCategory,
             starred: false
         }
+
         DLinks.push(newDlinks)
-        console.log("nuu linkaa", DLinks)
         const stringifyData = JSON.stringify(DLinks)
         
         localStorage.setItem('savelink-data', stringifyData)
-        setDialog(false)
 
+        setDialog(false)
     }
 
 
-    useEffect(() => {
-        const retrievedData : IData =  JSON.parse(localStorage.getItem('savelink-data'))
 
+
+
+    const [newCat, setNewCat] = useState<string | number>('')
+    const [category, setCategory] = useState([])
+
+    const createCategory = (e : React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        setCategory(prev => {
+         const nu = [...prev, newCat]
+         return nu;
+        })
+        const stringifyCat = JSON.stringify(category);
+
+        localStorage.setItem("savelink-catogories", stringifyCat)
+    }
+
+    useEffect(() => {
+        const retrievedData : IData =  JSON.parse(localStorage.getItem('savelink-data') || '{}')
         if(retrievedData === null) {
             const stringifyData = JSON.stringify(DLinks)
             localStorage.setItem('savelink-data', stringifyData)
         }
         setDLinks(retrievedData)
-        console.log(retrievedData)
     },[])
 
-    
-    const [newCategory, setNewCategory] = useState('');
-    const handleNewCategory = (e) => {
-        e.preventDefault()
-        setNewCategory(e.target.value)
-    }
+    useEffect(()=> {
+        const retrievedCat =  JSON.parse(localStorage.getItem('savelink-catogories') || '{}');
+        if(retrievedCat === null) {
+            setCategory(['default'])
+            const stringifyCat = JSON.stringify(category);
+            localStorage.setItem("savelink-catogories", stringifyCat)
+        }
+    },[])
+
 
 
     return (
@@ -106,8 +124,8 @@ const Dashboard:NextPage = () => {
 
             <section>
                 <div className={styles.create_category} >
-                    <input type="text" placeholder='you can select from preexisting categories' />
-                    <button onClick={handleNewCategory}>
+                    <input onChange={e => setNewCat(e.target.value)}  type="text" placeholder='you can select from preexisting categories' />
+                    <button onClick={createCategory}>
                         <span>+</span> Create category
                     </button>
                 </div>
@@ -116,7 +134,7 @@ const Dashboard:NextPage = () => {
                     <p>Select Category</p>
                     <select name = "categories">
                         <option value="">Choose...</option>
-                        {categories.map((e, index) => (
+                        {category.map((e, index) => (
                             <>
                                 <option key={index} value={e}> {e} </option>
                             </>
