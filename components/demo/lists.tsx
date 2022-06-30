32d396e4f36d5aeda07c3ac21b9fd1cc98296530
Phyf3 <phade075@gmail.com>
2022-06-30 22:33:09 +0100

    fit : adding links working in demo with local srorage ..fix TS error

---
+++ components/demo/lists.tsx
@@ -0,0 +1,76 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/list.module.css'
+import { FC, useEffect, useState } from 'react'
+import SvgComponent from '../svg/starsvg'
+
+interface IProps {
+    array : {
+    _id : number;
+    title : string;
+    url : string;
+    category : string;
+    }[]
+}
+
+const DemoList:FC<IProps> = ({array}) => {
+    const [bkmrkd, setBkmrkd] = useState(false)
+
+    function star() {
+        setBkmrkd(!bkmrkd)
+    }
+
+    const starProps = {
+        starred : bkmrkd
+    }
+
+    
+  return (
+    <>
+    {array?.map(data => (
+    <div key={data._id} className={styles.link_wrapper}>
+        <div className={[styles.link_list, styles.dark_scheme, styles.light_scheme].join(" ")}>
+            <div className={styles.links}>
+                <h3>{data.title}</h3>
+                <p> 
+                    {data.url.includes("http") ?
+                    <Link href={data.url}>
+                        {data.url}      
+                    </Link> 
+                    :
+                    <Link href = {`http://${data.url}`}>
+                        {`http://${data.url}`}
+                    </Link>
+                    }
+                    
+                </p>
+            </div>
+            <div className={styles.link__footer} >
+                <div className={styles.link__category}>
+                    <h3>{data.category}</h3>
+                </div>
+
+                <div  className={styles.link__images}>
+                    <div className={styles.link__image}
+                    onClick = {star} >
+                        <SvgComponent starred = {bkmrkd} />
+                    </div>
+
+                    <div className={styles.link__image}>
+                        <Image 
+                            src='/share.svg'
+                            alt='share button'
+                            width={20}
+                            height={20}
+                        />
+                    </div>
+                </div>    
+            </div>
+        </div>
+    </div>
+     ))}
+     </>
+  )
+}
+
+export default DemoList
--- pages/demo/dashboard.tsx
+++ pages/demo/dashboard.tsx
@@ -4,44 +4,98 @@ import styles from '../../styles/dashboard.module.css'
 import links from '../../data/dummydata.json' 
 // import Layout from '../components/layout'
 import { NextPage } from 'next'
-import { useRef, useState } from 'react'
+import { FormEvent, useEffect, useRef, useState } from 'react'
 import { Modal, Dialog } from 'react-dialog-polyfill'
+import DemoList from '../../components/demo/lists'
 
+interface IData {
+    _id : number;
+    title : string;
+    url : string;
+    category : string;
+    starred : boolean
+}
 
 const Dashboard:NextPage = () => {
-    const link = links.data
-    console.log(link)
+
+    const data : IData[] =  [
+        {
+            _id : 1,
+            title : "Title from the shared link will go here",
+            url : "https://link_you_want_to_save",
+            category : "category_set",
+            starred : true
+        }
+    ]
+
+    const [DLinks, setDLinks] = useState(data);
     let categories = []
-    for (let i = 0 ; i < link.length ; ++i) {
-        categories.push(link[i].category)
+    for (let i = 0 ; i < DLinks.length ; ++i) {
+        categories.push(DLinks[i].category)
     }
-    console.log(categories)
 
     const [dialog, setDialog] = useState(false);
 
-    function handleSubmit (e : React.SyntheticEvent) {
+    function handleSubmit (e : FormEvent<HTMLFormElement>) {
         e.preventDefault()
-
+        const data = new FormData(e.target);
+        let nutitle :string = data.get("title")?.toString() || ''
+        let nulink:string =  data.get("link")?.toString()!
+        const form = document.forms['input-form'].categories;
+        console.log(form)
+        let latestID = DLinks[DLinks.length - 1]._id++
+        
+        const newDlinks :IData = {
+            _id : Math.random(),
+            title : nutitle,
+            url: nulink,
+            category: form.value,
+            starred: false
+        }
+        DLinks.push(newDlinks)
+        console.log("nuu linkaa", DLinks)
+        const stringifyData = JSON.stringify(DLinks)
+        
+        localStorage.setItem('savelink-data', stringifyData)
         setDialog(false)
+
     }
+
+
+    useEffect(() => {
+
+
+        const retrievedData =  JSON.parse(localStorage.getItem('savelink-data'))
+
+        if(retrievedData === null) {
+            const stringifyData = JSON.stringify(DLinks)
+            localStorage.setItem('savelink-data', stringifyData)
+        }
+        setDLinks(retrievedData)
+        console.log(retrievedData)
+
+    },[])
+
+    
+
 
     return (
         <div className={styles.container}>
             <main className = {styles.main}>
                 <section>
-                    <List array = {link} />               
+                    <DemoList array = {DLinks}/>               
                 </section>  
             </main>
 
             <div>
       <Dialog className={styles.dialog} open={dialog}>
-        <form  onSubmit={handleSubmit} action="">
-            <input  placeholder='Title'/>
-            <input  placeholder='Link'/>
+        <form id = "input-form" onSubmit={handleSubmit} action="">
+            <input name = "title"  placeholder='Title'/>
+            <input name = "link" placeholder='Link' required />
 
             <p>Select Category</p>
-            <select>
-                <option value="default">Choose...</option>
+            <select name = "categories">
+                <option value="">Choose...</option>
                 {categories.map((e, index) => (
                     <>
                         <option key={index} value={e}> {e} </option>
