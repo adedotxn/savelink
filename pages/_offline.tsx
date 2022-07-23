ee390e2661a4312d77daec0383d087b09fe06a5a
Phyf3 <phade075@gmail.com>
2022-07-23 05:48:43 +0100

    prev commit

---
+++ pages/_offline.tsx
@@ -0,0 +1,9 @@
+export const Offline = () => {
+    return (
+        <div className="loading_container">
+            <div className="offlne">
+                <h2>Offline RN. Go online 🙂</h2>
+            </div>
+        </div>
+    )
+}
