456f3f56f1e0eb0c67d308c1bdb303049056c090
adedotxn <adedotxn.dev@gmail.com>
2023-01-24 15:00:04 +0100

    fix: next-pwa issue

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
-  pwa: {
+const withPWA =  require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
     runtimeCaching,
     disable: process.env.NODE_ENV === "development",
-  },
 })
+
+
+const nextConfig = withPWA({
+    // next config
+});
+
+module.exports = nextConfig;
