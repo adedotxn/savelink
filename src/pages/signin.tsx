2b8b237542ee9ea685fe38eb722618f37902654d
adedotxn <adedotxn.dev@gmail.com>
2023-07-12 14:50:47 +0100

    fix: font imports

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { CabinetGrotesk } from "@utils/font";
 import Image from "next/image";
+
 const SignIn = (): JSX.Element => {
   const { data: session, status } = useSession();
 
--- src/utils/font/index.ts
+++ src/utils/font/index.ts
@@ -3,34 +3,34 @@ import localFont from "next/font/local";
 export const CabinetGrotesk = localFont({
   src: [
     {
-      path: "../../../public/fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Light.otf",
+      path: "./fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Light.otf",
       weight: "300",
       style: "normal",
     },
 
     {
-      path: "../../../public/fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Regular.otf",
+      path: "./fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Regular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Medium.otf",
+      path: "./fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Medium.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Bold.otf",
+      path: "./fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Bold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../../public/fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-ExtraBold.otf",
+      path: "./fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-ExtraBold.otf",
       weight: "800",
       style: "normal",
     },
 
     {
-      path: "../../../public/fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Black.otf",
+      path: "./fonts/CabinetGrotesk/Fonts/OTF/CabinetGrotesk-Black.otf",
       weight: "900",
       style: "normal",
     },
